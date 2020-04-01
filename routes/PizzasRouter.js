3a460bc2f018dbd902201f413b4fafaadb0f987b
Rafael Santana <raf.santa11@gmail.com>
2020-04-01 20:54:28 -0300

    Implementando crud das pizzas

--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 const PizzasController = require('../controllers/PizzasController');
-const PedidosController = require('../controllers/PedidosController');
 
 /* GET home page. */
 router.get('/', PizzasController.index);
-router.get('/pizzas/busca', PizzasController.search);
-router.get('/pizzas/:id', PizzasController.show);
-router.post('/pedidos/add', PedidosController.add);
-
+router.get('/busca',PizzasController.search);
+router.get('/pizzas/:id/edit', PizzasController.edit);
+router.get('/pizzas/create', PizzasController.create);
+router.get('/pizzas/:id',PizzasController.show);
+router.get('/pizzas',PizzasController.list);
+router.post('/pizzas',PizzasController.store);
+router.put("/pizzas/:id/update", PizzasController.update);
+router.delete("/pizzas/:id", PizzasController.delete);
 
 module.exports = router;
